60daf79acd41e4d96095703fcf2256b17613d941
7yl4r <7yl4r@users.noreply.github.com>
2025-10-30 16:46:25 +0000

    deploy: cdde8a68ef9a0fe41fe7f601cdca939e3edf4b77

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '0.1.1.dev617+gd540b66fd',
+    VERSION: '0.1.1.dev618+gcdde8a68e',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
