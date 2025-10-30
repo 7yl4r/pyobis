d162626d71cd4c382f6f91b394eda61ab0555e86
7yl4r <7yl4r@users.noreply.github.com>
2025-10-30 16:45:10 +0000

    deploy: d540b66fd8113a4b5864f74a04412c4f9d9c46ec

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,5 @@
-var DOCUMENTATION_OPTIONS = {
-    URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.2.5',
+const DOCUMENTATION_OPTIONS = {
+    VERSION: '0.1.1.dev617+gd540b66fd',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
